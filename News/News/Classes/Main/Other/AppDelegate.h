fddeb43f82aeab0da0c491a973636f8b5826832f
DaWangXXX <hxiang1118@163.com>
2015-09-14 19:54:04 +0800

    项目初始化，添加第三方库

---
+++ News/News/Classes/Main/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  News
+//
+//  Created by XXX on 15/9/14.
+//  Copyright (c) 2015年 huangx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ News/News/Classes/Main/Other/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  News
+//
+//  Created by XXX on 15/9/14.
+//  Copyright (c) 2015年 huangx. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+
+
+@end
